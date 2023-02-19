06c005e9542522cac67eefe8dc8ffc5af320e445
ZoDD-Script <blessedchukwu89@gmail.com>
2023-02-19 13:03:37 -0800

    handled error correctly

--- controller/uploadController.js
+++ controller/uploadController.js
@@ -4,7 +4,7 @@ const uploadController = async (req, res, next) => {
   const data = await uploadService(req, res)
 
   console.log("data", data)
-  if (!data?.success) return res.status(400).json({message: 'Failed to upload file'})
+  if (!data?.success) return res.status(400).json(data)
 
   return res.status(200).json(data)
 }
